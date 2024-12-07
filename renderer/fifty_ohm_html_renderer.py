278605fc28ff9f7c1a68566ca6915c505f47c3a1
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2024-12-07 20:20:49 +0100

    feature: First tag implementation

--- renderer/fifty_ohm_html_renderer.py
+++ renderer/fifty_ohm_html_renderer.py
@@ -1,12 +1,14 @@
 from mistletoe import HtmlRenderer
 
 from renderer.comment import Comment
+from renderer.tag import Tag
 from renderer.quote import Quote
+from renderer.underline import Underline
 
 
 class FiftyOhmHtmlRenderer(HtmlRenderer):
     def __init__(self):
-        super().__init__(Comment, Quote)
+        super().__init__(Comment, Quote, Underline, Tag)
 
     def render_comment():
         return None
@@ -16,3 +18,6 @@ class FiftyOhmHtmlRenderer(HtmlRenderer):
 
     def render_underline(self, token):
         return f"<u>{self.render_inner(token)}</u>"
+
+    def render_tag(self, token):
+        return f'<div class="{token.tagtype}">{self.render_inner(token)}</div>'
