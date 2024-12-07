d72917335f5166c61ca315495ffe1dce0a462d39
Johanna Riedel <Blattsee@gmx.de>
2024-12-08 00:54:59 +0100

    feat: Renderer add interim references with default values

--- renderer/fifty_ohm_html_renderer.py
+++ renderer/fifty_ohm_html_renderer.py
@@ -4,11 +4,12 @@ from renderer.comment import Comment
 from renderer.quote import Quote
 from renderer.halfwidth_spaces import HalfwidthSpaces
 from renderer.nonbreaking_spaces import NonbreakingSpaces, NonbreakingSpacesDots
+from renderer.references import References
 
 
 class FiftyOhmHtmlRenderer(HtmlRenderer):
     def __init__(self):
-        super().__init__(Comment, Quote, HalfwidthSpaces, NonbreakingSpaces, NonbreakingSpacesDots)
+        super().__init__(Comment, Quote, HalfwidthSpaces, NonbreakingSpaces, NonbreakingSpacesDots, References)
 
     def render_comment():
         return None
@@ -27,3 +28,6 @@ class FiftyOhmHtmlRenderer(HtmlRenderer):
 
     def render_nonbreaking_spaces_dots(self, token):
         return f"&#160;{token.first}&#160;"
+
+    def render_references(self, token, dest_id=0, chap_url="DeineMama.html"):
+        return f"<a href=&#34;{chap_url}#ref_{token.first}&#34; onclick=&#34;highlightRef(&#39;{token.first}&#39;);&#34;>{dest_id}</a>"
