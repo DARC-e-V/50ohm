f05e5e9a2af7ae85cd3748a2e163e61840e46e04
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2024-12-07 15:04:16 +0100

    feature: add util function for testing

--- renderer/fifty_ohm_html_renderer.py
+++ renderer/fifty_ohm_html_renderer.py
@@ -1,12 +1,15 @@
 from mistletoe import HtmlRenderer
 
 from renderer.comment import Comment
+from renderer.quote import Quote
 
 
 class FiftyOhmHtmlRenderer(HtmlRenderer):
     def __init__(self):
-        super().__init__(Comment)
+        super().__init__(Comment, Quote)
 
     def render_comment():
         return None
 
+    def render_quote(self, token):
+        return f"„{self.render_inner(token)}“"
