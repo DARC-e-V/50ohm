de782b53766ff58e613716848544f3bcb5ae888a
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2024-12-07 22:36:25 +0100

    feature: added thematic break and all tags

--- renderer/fifty_ohm_html_renderer.py
+++ renderer/fifty_ohm_html_renderer.py
@@ -1,4 +1,5 @@
 from mistletoe import HtmlRenderer
+import textwrap
 
 from renderer.comment import Comment
 from renderer.tag import Tag
@@ -7,6 +8,10 @@ from renderer.underline import Underline
 
 
 class FiftyOhmHtmlRenderer(HtmlRenderer):
+
+    margin_anchor_id = 0
+    margin_id = 0
+
     def __init__(self):
         super().__init__(Comment, Quote, Underline, Tag)
 
@@ -19,5 +24,57 @@ class FiftyOhmHtmlRenderer(HtmlRenderer):
     def render_underline(self, token):
         return f"<u>{self.render_inner(token)}</u>"
 
+    def render_thematic_break(self, token):
+        self.margin_anchor_id += 1
+        return f'<a id="margin_{self.margin_anchor_id}"></a>'
+
+    @staticmethod
+    def render_tag_helper(type, content, margin_id, margin_anchor_id):
+        """ This function is used to render the different types of tags. It is
+        used in the HtmlRenderer class and also in the test class """
+        return textwrap.dedent(f'''\
+            <div class="margin {type}" id="margin_for_{margin_id}">
+                {content}
+            </div>
+            <a id="margin_orig_{margin_id}"></a>
+            <script>
+            (() => {{
+                function moveFunc() {{
+                    const targetEl = document.getElementById(
+                        window.innerWidth > 800 ? "margin_{margin_anchor_id}" : "margin_orig_{margin_id}"
+                    );
+                    targetEl.parentNode.insertBefore(
+                        document.getElementById("margin_for_{margin_id}"),
+                        targetEl
+                    );
+                }}
+                addEventListener("resize", moveFunc);
+                moveFunc();
+            }})();
+            </script>
+        ''')
+
     def render_tag(self, token):
-        return f'<div class="{token.tagtype}">{self.render_inner(token)}</div>'
+        if(token.tagtype == "latexonly"):
+            return ""
+        elif(token.tagtype == "webonly"):
+            return self.render_inner(token)
+
+        self.margin_id += 1
+
+        if(token.tagtype == "webmargin"):
+            type = "margin"
+        elif(token.tagtype == "webtip"):
+            type = "tipp"
+        elif(token.tagtype == "webindepth"):
+            type = "indepth"
+        else :
+            type = token.tagtype
+
+        return self.render_tag_helper(
+            type,
+            self.render_inner(token),
+            self.margin_id,
+            self.margin_anchor_id
+        )
+    
