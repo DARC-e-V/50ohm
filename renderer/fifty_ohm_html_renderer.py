d8670daf637ffdc161b4799845505ce50b1ea5af
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2024-12-08 13:56:24 +0100

    bugfix: tipp vs. tipp

--- renderer/fifty_ohm_html_renderer.py
+++ renderer/fifty_ohm_html_renderer.py
@@ -74,8 +74,8 @@ class FiftyOhmHtmlRenderer(HtmlRenderer):
 
         if(token.tagtype == "webmargin"):
             type = "margin"
-        elif(token.tagtype == "webtipp"):
-            type = "tipp"
+        elif(token.tagtype == "webtip"):
+            type = "tip"
         elif(token.tagtype == "webindepth"):
             type = "indepth"
         else :
