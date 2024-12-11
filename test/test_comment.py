90556a59626714212e7be4c5b294e27380a8fc19
Tim Grelka <grelka@indibit.eu>
2024-12-11 01:09:53 +0100

    refactor: use paragraph test helper

--- test/test_comment.py
+++ test/test_comment.py
@@ -2,18 +2,19 @@ import mistletoe
 
 from renderer.fifty_ohm_html_renderer import FiftyOhmHtmlRenderer
 from renderer.fifty_ohm_latex_renderer import FiftyOhmLaTeXRenderer
+from test.util import paragraph
 
 
 def test_comment_html():
     assertions = {
-        "%Comment\nBar": "<p>Bar</p>\n",
+        "%Comment\nBar": "Bar",
         # TODO: This test is not working as expected. The comment should be removed
         # "%Comment\n" : "a",
-        "Foo\n%Comment\nBar": "<p>Foo\nBar</p>\n",
+        "Foo\n%Comment\nBar": "Foo\nBar",
     }
 
     for assertion in assertions:
-        assert mistletoe.markdown(assertion, FiftyOhmHtmlRenderer) == assertions[assertion]
+        assert mistletoe.markdown(assertion, FiftyOhmHtmlRenderer) == paragraph(assertions[assertion])
 
 
 def test_comment_latex():
