fa3ba2973bc96c09abe93bcaaa5edb556d809bd5
Tim Grelka <grelka@indibit.eu>
2025-02-16 17:56:26 +0100

    fix: add line break after LaTeX comment

--- test/test_comment.py
+++ test/test_comment.py
@@ -23,10 +23,10 @@ def test_comment_html():
 @pytest.mark.latex
 def test_comment_latex():
     assertions = {
-        "%Comment\nBar": "% Comment\nBar\n",
-        "%Comment\n": "% Comment",
+        "%Comment\nBar": "% Comment\n\nBar\n",
+        "%Comment\n": "% Comment\n",
         "Foo 100 % Bar": "\nFoo 100 \% Bar\n",
-        "Foo\n%Comment\nBar": "\nFoo\n% Comment\nBar\n",
+        "Foo\n%Comment\nBar": "\nFoo\n% Comment\n\nBar\n",
     }
 
     for assertion in assertions:
