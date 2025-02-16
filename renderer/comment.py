c6d167331be97d9d2fe3ef6c6f6f17f1d4f3a24e
Tim Grelka <grelka@indibit.eu>
2025-02-16 18:48:51 +0100

    refactor: remove unused code

--- renderer/comment.py
+++ renderer/comment.py
@@ -3,14 +3,6 @@ import re
 from mistletoe.block_token import BlockToken
 from mistletoe.span_token import SpanToken
 
-# class SpanComment(SpanToken):
-#     """
-#     Coment span token (% Eiersalat).
-#     Identifies a comment in text.
-#     """
-#
-#     pattern = re.compile(r"(%.*\n?)")
-
 
 class BlockComment(BlockToken):
     def __init__(self, match):
