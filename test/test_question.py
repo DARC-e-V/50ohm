1434519cbf52fc36e52f5acccc62a799c8192f30
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-02-17 16:59:50 +0100

    fixed: test names for question-test

--- test/test_question.py
+++ test/test_question.py
@@ -9,7 +9,7 @@ from test.util import paragraph
 
 
 @pytest.mark.html
-def test_comment_html():
+def test_question_html():
     assertions = {
         "[question:123]": "123\n",
         "\n[question:123]\n": "123\n",
@@ -25,7 +25,7 @@ def test_comment_html():
             assert renderer.render(Document(assertion)) == assertions[assertion]
 
 @pytest.mark.html
-def test_comment_translation_html():
+def test_question_translation_html():
     assertions = {
         "[question:123]": "ED111\n",
         "\n[question:123]\n": "ED111\n",
@@ -44,16 +44,16 @@ def test_comment_translation_html():
 
 
 @pytest.mark.latex
-def test_comment_latex():
+def test_question_latex():
     assertions = {
-        "[question:123]": "123",
-        "\n[question:123]\n": "123",
-        "\n[question:123]\n[question:123]": "123123",
-        "Foo\n[question:123]\nBar": "\nFoo\n123\nBar\n",
+        "[question:123]": "\question{123}",
+        "\n[question:123]\n": "\question{123}",
+        "\n[question:123]\n[question:123]": "\question{123}\question{123}",
+        "Foo\n[question:123]\nBar": "\nFoo\n\question{123}\nBar\n",
     }
 
     def test_function(input):
-        return f"{input}"
+        return "\\question{"+input+"}"
 
     with FiftyOhmLaTeXRenderer(test_function) as renderer:
         for assertion in assertions:
