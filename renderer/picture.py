039eae89565d72a56d4264b632717a2a48ad390a
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-02-26 21:23:03 +0100

    First draft for pictures

---
+++ renderer/picture.py
@@ -0,0 +1,19 @@
+import re
+
+from mistletoe.span_token import SpanToken
+
+
+class Picture(SpanToken):
+    """
+    Span token for pictures.
+    Example: [picture:727:amplitude_periode_halbwellen:Positive und negative Halbwellen einer Sinusschwingung]
+    """
+
+    parse_inner = False
+    pattern = re.compile(r"\[picture:(\d+):(.+):(.+)\]")
+
+    def __init__(self, match_object):
+        self.id = match_object.group(1)
+        self.ref = match_object.group(2)
+        self.text = match_object.group(3)
+        self.number = "TODO"
