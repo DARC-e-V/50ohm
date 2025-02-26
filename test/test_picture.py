09758ad306d2ab535d84cf6a546749945a1c312e
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-02-26 21:23:03 +0100

    First draft for pictures

---
+++ test/test_picture.py
@@ -0,0 +1,27 @@
+import mistletoe
+import pytest
+
+from renderer.fifty_ohm_html_renderer import FiftyOhmHtmlRenderer
+from test.util import paragraph
+
+
+@pytest.mark.html
+def test_underline_html():
+    assertions = {
+        "[picture:123:abc:Text]": FiftyOhmHtmlRenderer.render_picture_helper("123", "abc", "Text", "TODO"),
+    }
+
+    for assertion in assertions:
+        assert mistletoe.markdown(assertion, FiftyOhmHtmlRenderer) == paragraph(assertions[assertion])
+
+
+#@pytest.mark.latex
+#def test_underline_latex():
+#    assertions = {
+#        "<u>Text</u>": "\n\\underline{Text}\n",
+#        "<u> Text Text </u>": "\n\\underline{ Text Text }\n",
+#        "<u> Text *Text* </u>": "\n\\underline{ Text \emph{Text} }\n",
+#    }
+#
+#    for assertion in assertions:
+#        assert mistletoe.markdown(assertion, FiftyOhmLaTeXRenderer) == assertions[assertion]
