74859151147a2804a612e43bf2fe00befc689f8c
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-05-14 15:55:32 +0200

    fix: fixed bug in morse code

--- test/test_morse.py
+++ test/test_morse.py
@@ -8,11 +8,11 @@ from test.util import paragraph
 @pytest.mark.html
 def test_morse_html():
     assertions = {
-        '"[morse:a]"': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2]]),
-        '"[morse: a]"': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2]]),
-        '"[morse: aaa]"': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2], [1, 2], [1, 2]]),
-        '"[morse: a a]"': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2], [3], [1, 2]]),
-        '"[morse: db0]"': FiftyOhmHtmlRenderer.render_morse_helper([[2, 1, 1], [2, 1, 1, 1], [2, 2, 2, 2, 2]]),
+        '[morse:a]': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2]]),
+        '[morse: a]': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2]]),
+        '[morse: aaa]': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2], [1, 2], [1, 2]]),
+        '[morse: a a]': FiftyOhmHtmlRenderer.render_morse_helper([[1, 2], [3], [1, 2]]),
+        '[morse: db0]': FiftyOhmHtmlRenderer.render_morse_helper([[2, 1, 1], [2, 1, 1, 1], [2, 2, 2, 2, 2]]),
     }
 
     for assertion in assertions:
