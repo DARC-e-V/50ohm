b61efacbbce4299cff12e240a1d5c141abdbcdcf
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-05-18 10:17:41 +0200

    feature: added code to provide first data download

--- build.py
+++ build.py
@@ -7,6 +7,10 @@ conf = config.Config()
 api  = directus.DirectusAPI(conf)
 dl   = download.Download(api)
 
+# Download the text files:
 dl.download_edition("N")
+
+# Download additional files:
 dl.download_question_metadata()
 dl.download_photos()
+dl.download_pictures()
