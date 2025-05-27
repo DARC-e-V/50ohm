9833ae4fb3aeb3be03e0c7da1119373521f83200
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-05-27 22:17:39 +0200

    feature: add the ability to talk to different directus instances

--- build.py
+++ build.py
@@ -4,7 +4,8 @@ import src.config as config
 import src.download as download
 
 conf = config.Config()
-api = directus.DirectusAPI(conf)
+api = directus.DirectusAPI(conf.question_base_url, conf.question_access_token)
+content_api = directus.DirectusAPI(conf.content_base_url, conf.content_access_token)
 dl = download.Download(api, conf)
 bd = build.Build()
 
