cf089ce6603305e4f97c0206c0de005ac8c8641b
Prof. Dr. Matthias Jung DL9MJ <dl9mj@darc.de>
2025-06-01 14:58:06 +0200

    feature: support qso in slides

--- renderer/fifty_ohm_html_slide_renderer.py
+++ renderer/fifty_ohm_html_slide_renderer.py
@@ -1,7 +1,9 @@
+from jinja2 import Environment, FileSystemLoader
 from mistletoe import Document
 
 from renderer.fifty_ohm_html_renderer import FiftyOhmHtmlRenderer
 
+from .qso import Qso
 from .slide_break import SlideBreak
 
 
@@ -10,10 +12,12 @@ class FiftyOhmHtmlSlideRenderer(FiftyOhmHtmlRenderer):
     def __init__(self, question_renderer=None, picture_handler=None, photo_handler=None):
         super().__init__(
             SlideBreak,
+            Qso,
             question_renderer=question_renderer,
             picture_handler=picture_handler,
             photo_handler=photo_handler,
         )
+        self.env = Environment(loader=FileSystemLoader("templates/slide"))
     
     def render_wrapper(self, content):
         content = "---\n" + content
@@ -32,4 +36,12 @@ class FiftyOhmHtmlSlideRenderer(FiftyOhmHtmlRenderer):
             return f'<section data-background-color="#40C08C">\n{inner}\n</section>\n'
         else:
             return f'<section {token.attribute}>\n{inner}\n</section>\n'
+
+    def render_qso(self, token):
+        qso = ""
+        for child in token.children:
+            direction = "other" if child.received else "own"
+            fade = "fragment fade-left" if child.received else "fragment fade-right"
+            qso += f'<div class="qso_{direction} {fade}">{self.render_inner(child)}</div>\n'
+        return qso
 
