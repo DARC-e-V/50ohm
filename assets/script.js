68190220a4211d8966f8aeed0e0f95e6e64724bf
Matthias Jung <m.jung@uni-wuerzburg.de>
2026-01-04 16:35:34 +0400

    feature: add qtyrange command

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var macros = {
     ",": (context) => context.future()?.text === " " ? "{\\char`,}" : "\\char`,",
     "\\qty": "{#1\\,\\mathrm{#2}}",
+    "\\qtyrange": "{#1 - #2\\,\\mathrm{#3}}",
     "\\unit": "{\\mathrm{#1}}",
     "\\squared": "{^{2}}",
     "\\cubed": "{^{3}}",
