752ca35da7704da97e1b33db0201994d67fae7e3
Matthias Jung <m.jung@uni-wuerzburg.de>
2026-02-06 23:46:12 +0100

    refactor: remove download

--- src/config.py
+++ src/config.py
@@ -13,15 +13,22 @@ class Config:
         else:
             self.config = {}
 
-        self.git_url = self.get_config_value("git_url", "git@github.com:DARC-e-V/50ohm-contents-dl.git")
+        self.p_data = Path(self.get_config_value("content_path", "/var/www/content"))
 
-        self.p_fragenkatalog = Path(
-            self.get_config_value("path_fragenkatalog", "./data/git_content/contents/questions/fragenkatalog3b.json")
-        )
+        self.p_data_toc = self.p_data / "toc"
 
-        self.p_data = Path("./data")
-        self.p_data_photos = self.p_data / "git_content" / "contents" / "photos"
-        self.p_data_pictures = self.p_data / "git_content" / "contents" / "drawings"
+        self.p_data_contents = self.p_data / "contents"
+        self.p_data_questions = self.p_data_contents / "questions"
+        self.p_data_html = self.p_data_contents / "html"
+        self.p_data_photos = self.p_data_contents / "photos"
+        self.p_data_pictures = self.p_data_contents / "drawings"
+        self.p_data_sections = self.p_data_contents / "sections"
+        self.p_data_slides = self.p_data_contents / "slides"
+        self.p_data_snippets = self.p_data_contents / "snippets"
+        self.p_data_static = self.p_data_contents / "static"
+
+        self.p_data_fragenkatalog = self.p_data_questions / "fragenkatalog3b.json"
+        self.p_data_metadata = self.p_data_questions / "metadata3b.json"
 
         self.p_build = Path("./build")
         self.p_build_photos = self.p_build / "photos"
--- test/test_html.py
+++ test/test_html.py
@@ -5,16 +5,13 @@ from jinja2 import Environment, FileSystemLoader
 from mistletoe import Document
 
 import src.config as config
-import src.download as download
 from renderer.fifty_ohm_html_renderer import FiftyOhmHtmlRenderer
 
 conf = config.Config()
-dl = download.Download(conf)
-dl.download_git_content()
 
 
 def parse_katalog():
-    with open("data/git_content/contents/questions/fragenkatalog3b.json") as fragenkatalog_file:
+    with open(conf.p_data_fragenkatalog) as fragenkatalog_file:
         fragenkatalog = json.load(fragenkatalog_file)
 
         questions = {}
@@ -39,7 +36,7 @@ questions = parse_katalog()
 
 def question_stub(number):
     """Combines the original question dataset from BNetzA with our internal metadata"""
-    with open("data/git_content/contents/questions/metadata3b.json") as metadata_file:
+    with open(conf.p_data_metadata) as metadata_file:
         metadata = json.load(metadata_file)
         question = questions[number]
         metadata = metadata[f"{number}"]
